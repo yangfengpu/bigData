1d2d3e2f5cb6291f8911c21f86ef0d90383013d0
yangfengpu <fengpuy@gmail.com>
2015-06-01 17:25:40 +0800

    max code line length test

--- cococloud/models.py
+++ cococloud/models.py
@@ -6,9 +6,12 @@ class TripleContext(models.Model):
         return self.name
 
 class KeyVal(models.Model):
-    container     = models.ForeignKey(TripleContext, db_index=True)
-    key           = models.CharField(max_length=240, db_index=True)
-    valueDescr    = models.CharField(max_length=240, db_index=True)
+    container     = models.ForeignKey(TripleContext, \
+                                                                  db_index=True)
+    key           = models.CharField(max_length=240,\
+                                                           db_index=True)
+    valueDescr    = models.CharField(max_length=240, \
+                                                                  db_index=True)
 
 class Tag(models.Model):
     tag           = models.CharField(max_length=200, unique=True)
@@ -25,12 +28,15 @@ class LiteralType(models.Model):
 
 class EducocoNode(models.Model): # This is the vocabulary
     description   = models.TextField()
-    showName      = models.CharField(max_length = 100)
+    showName    = models.CharField(max_length = 100)
     nodeType      = models.CharField(max_length = 100) # Literal or URIRef
-    literalType   = models.ForeignKey(LiteralType)
+    literalType    = models.ForeignKey(LiteralType)
     uriDomain     = models.CharField(max_length=300)
-    uriIdentifier = models.CharField(max_length=500,  unique=True) #full_name
-    tag           = models.ManyToManyField(Tag, related_name='vocabulary_tag', db_index=True)
+    uriIdentifier  = models.CharField(max_length=500, \
+                                                                unique=True) #full_name
+    tag                  = models.ManyToManyField(Tag, \
+                                                  related_name='vocabulary_tag', \
+                                                  db_index=True)
     def __str__(self): 
         return self.uriIdentifier
 
@@ -38,9 +44,16 @@ class EducocoNode(models.Model): # This is the vocabulary
 class EducocoTriplePattern(models.Model):
     name          = models.CharField(max_length = 100)
     description   = models.TextField()
-    subject       = models.ForeignKey(EducocoNode, related_name='triple_subject', db_index=True)
-    predicate     = models.ForeignKey(EducocoNode, related_name='triple_predicate', db_index=True)
-    subject       = models.ForeignKey(EducocoNode, related_name='triple_object', db_index=True)
-    context       = models.ForeignKey(TripleContext, related_name='triple_context')
+    subject       = models.ForeignKey(EducocoNode, \
+                                      related_name='triple_subject',\
+                                      db_index=True)
+    predicate     = models.ForeignKey(EducocoNode, \
+                                      related_name='triple_predicate', \
+                                      db_index=True)
+    subject       = models.ForeignKey(EducocoNode, \
+                                      related_name='triple_object', \
+                                      db_index=True)
+    context       = models.ForeignKey(TripleContext, \
+                                      related_name='triple_context')
     def __str__(self): 
         return self.name
